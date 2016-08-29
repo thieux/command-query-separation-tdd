50dd5766b0e9d461bc926ad6d6d17202b5b55fd3
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-29 22:50:51 +0200

    Build an application sample on top of add along its functional test

--- src/test/java/com/mathieupauly/cmdquerysep/CommandQuerySeparationTest.java
+++ src/test/java/com/mathieupauly/cmdquerysep/CommandQuerySeparationTest.java
@@ -7,6 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CommandQuerySeparationTest {
 
     @Test
+    public void integration_test() {
+        assertThat(application("1 2")).isEqualTo("3");
+    }
+
+    private String application(String pair) {
+        final String[] elements = pair.split(" ");
+        final int left = Integer.parseInt(elements[0]);
+        final int right = Integer.parseInt(elements[1]);
+
+        return String.valueOf(add(left, right));
+    }
+
+    @Test
     public void add_1_2() {
         assertThat(add(1, 2)).isEqualTo(3);
     }
