47f5473fd286744bfffbcc95e7d90581d98a6e26
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-08-29 22:42:40 +0200

    Create characterization test for 1+2

---
+++ src/test/java/com/mathieupauly/cmdquerysep/CommandQuerySeparationTest.java
@@ -0,0 +1,13 @@
+package com.mathieupauly.cmdquerysep;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommandQuerySeparationTest {
+
+    @Test
+    public void add_1_2() {
+        assertThat(1 + 2).isEqualTo(3);
+    }
+}
